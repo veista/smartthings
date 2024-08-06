e08fe83f72120f5d4aa3d1060c6e1fdcd202ad59
cgraf <2675875+cgraf@users.noreply.github.com>
2024-08-06 08:57:52 -0500

    Update switch.py
    Add rapid freeze, rapid cool and icemaker to TP1X_REF_21K

--- custom_components/smartthings/switch.py
+++ custom_components/smartthings/switch.py
@@ -146,7 +146,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                         )
                     ]
                 )
-            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K"):
+            elif model in ("21K_REF_LCD_FHUB6.0", "ARTIK051_REF_17K","TP1X_REF_21K"):
                 switches.extend(
                     [
                         SamsungOcfSwitch(
